c02e02c145a12b49bd78372aa1764d14dc7550da
unknown <jorn1013@gmail.com>
2023-11-13 21:09:17 +0700

    Some more fiddles, enough for today, quiz tmr, better rest well

--- parameters.py
+++ parameters.py
@@ -11,11 +11,11 @@ diagonal_distance = 40
 min_diagonal_distance = 20
 unit_mod = 6480
 start_offset_x = 0
-start_offset_z = -10
+start_offset_z = 10
 expandedness = 1 #basically idk what to call the span to x vs span to z
 
-max_diags = 5
-max_straights = 5
+max_diags = 10
+max_straights = 10
 
 h_edge = 150/unit_mod
 v_edge = 100/(unit_mod*expandedness)
@@ -40,7 +40,7 @@ bad = -10
 good = 2
 
 #camera
-eye = glm.vec3(0.0, 2.0, 12.0)
+eye = glm.vec3(0.0, 3.0, 12.0)
 center = glm.vec3(0.0, 1.0, 0.0)
 up = glm.vec3(0.0, 1.0, 0.0)
 
